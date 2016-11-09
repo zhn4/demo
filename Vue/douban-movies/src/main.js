7e5e1d74f3ff3ebbb7e160965286f15bbb82d1db
zhou <zhouhaonan4@gmail.com>
2016-11-09 22:35:59 +0800

    modify

--- Vue/douban-movies/src/main.js
+++ Vue/douban-movies/src/main.js
@@ -1,6 +1,15 @@
 import Vue from 'vue'
 import App from './App.vue'
 
+import VueRouter from "vue-router";
+import VueResource from 'vue-resource'
+
+//开启debug模式
+Vue.config.debug = true;
+
+Vue.use(VueRouter);
+Vue.use(VueResource);
+
 new Vue({
   el: '#app',
   render: h => h(App)
