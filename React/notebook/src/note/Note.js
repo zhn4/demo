e0c034a2f1936d5e7b205c713008fe2105936579
zhou <zhouhaonan4@gmail.com>
2017-02-21 15:02:11 +0800

    优化代码

--- React/notebook/src/note/Note.js
+++ React/notebook/src/note/Note.js
@@ -2,32 +2,12 @@ import React, { Component } from 'react';
 
 import InpuText from './component/InpuText';
 
-// let note = {
-//   details: [
-//     {
-//       text: '第一'
-//     },
-//     {
-//       text: '第二'
-//     },
-//     {
-//       text: '第三'
-//     }
-//   ]
-// }
-
-// let note = JSON.parse(localStorage.getItem('note'))
-
-// localStorage.setItem('note', JSON.stringify(note));
-
 class Note extends Component {
   constructor(props) {
     super(props);
     this.state = {
       inputdata: '还没输入',
-      // noteData: note.details
       noteData: ''
-      // noteData: JSON.parse(localStorage.getItem('note')).details
     }
   }
   componentDidMount() {
@@ -36,35 +16,16 @@ class Note extends Component {
   componentWillMount() {
     console.log('未加载组件');
     if(localStorage.getItem('note')) {// 判断时会否存在localStorage数据，有就返回给state
-      // console.log('有数据')
-      // console.log(JSON.parse(localStorage.getItem('note')))
       this.setState({
         noteData: JSON.parse(localStorage.getItem('note')).details
       })
-    }else {
-      console.log('没有数据')
-      // let note = {
-      //   details: []
-      // }
-      // this.setState({
-      //   noteData: note.details
-      // })
     }
   }
   getInputValue(getValue) {
-    this.setState({
+    this.setState({//输出输入数据，调试用
       inputdata: getValue
     })
-    // console.log(this.state.noteData)
-    // 输入的数据加入localStorage数据数组
-    // this.state.noteData.push({
-    //   text: getValue
-    // })
-    // let note = {
-    //   details: this.state.noteData
-    // }
-    // // console.log(note)
-    // localStorage.setItem('note', JSON.stringify(note));
+    //判断是否存在localStorage数据
     if(localStorage.getItem('note')) {
       console.log('已存在数据')
       this.state.noteData.push({
@@ -86,7 +47,6 @@ class Note extends Component {
         noteData: note.details
       })
       localStorage.setItem('note', JSON.stringify(note));
-
     }
   }
   render() {
@@ -94,7 +54,7 @@ class Note extends Component {
       <div>
         notepad
         <InpuText getInputValue={this.getInputValue.bind(this)}/>
-        <p>input data:{this.state.inputdata}</p>
+        {/* <p>input data:{this.state.inputdata}</p> */}
         <div>todo</div>
         {
           this.state.noteData
--- React/notebook/src/note/component/InpuText.js
+++ React/notebook/src/note/component/InpuText.js
@@ -1,36 +1,12 @@
 import React, { Component } from 'react';
 
-// let note = {
-//   details: [
-//     {
-//       id: 0,
-//       text: '第一条数据'
-//     },
-//     {
-//       id: 1,
-//       text: '第二条数据'
-//     }
-//   ]
-// }
-// localStorage.setItem('note', JSON.stringify(note));//保存
-// console.log(localStorage.getItem('note'));//读取
-// console.log(JSON.parse(localStorage.getItem('note')));//读取
-
 class InpuText extends Component {
   constructor(props) {
     super(props);
     this.state = {
       inputValue: '',
-      // noteData: note.details
-      // noteData: JSON.parse(localStorage.getItem('note'))
     }
   }
-  // componentDidMount() {
-  //   console.log('已加载组件，读取数据');
-  // }
-  // componentWillMount() {
-  //   console.log('未加载组件');
-  // }
   updateInputValue(e) {
     this.setState({
       inputValue: e.target.value
