dd2c1ef9ad908c9f8ba1ff93bbcddf2f305297e2
zhou <zhouhaonan4@gmail.com>
2017-03-03 15:14:09 +0800

    增加添加数据功能

--- React/notebook/src/note/Judge.js
+++ React/notebook/src/note/Judge.js
@@ -1,75 +1,107 @@
 import React, { Component } from 'react';
 
 import Item from './component/Item';
+import InpuText from './component/InpuText';
 
-let list = [
-  {
-    id: 1,
-    text: 'one thing',
-    done: false
-  },
-  {
-    id: 2,
-    text: 'two thins',
-    done: true
-  },
-  {
-    id:3,
-    text: 'three thing',
-    done: false
-  },
-  {
-    id: 4,
-    text: 'four thing',
-    done: false
-  }
-]
+import './style/Note.css';
+
+// let list = [
+//   {
+//     text: 'one thing',
+//     done: false
+//   },
+//   {
+//     text: 'two thins',
+//     done: true
+//   },
+//   {
+//     text: 'three thing',
+//     done: false
+//   },
+//   {
+//     text: 'four thing',
+//     done: false
+//   }
+// ]
+
+// localStorage.setItem('list', JSON.stringify(list));
+
+let list = JSON.parse(localStorage.getItem('list'));// localStorage数据
+// console.log('list')
+// console.log(list)
 
 class Judge extends Component {
   constructor(props) {
     super(props)
     this.state = {
-      list: list,
-      text: '123'
+      list: '',
+      text: '还没数据'
     }
   }
   componentWillMount() {
-    console.log(this.state.list);
-    let data = {
-      todo: [],
-      done: []
-    }
-    console.log(data)
-    this.state.list.map((details, i)=> (
-      console.log(details)
-    ))
-  }
-  handleChecked(sendData) {
-    list[sendData].done = !list[sendData].done
-    this.setState({
+    // console.log(this.state.list);
+    // this.state.list.map((details, i)=> (
+    //   console.log(details)
+    // ));
+    this.setState({// 载入localStorage数据，修改state
       list: list
     })
+  }
+  getInputValue(getValue) {
+    console.log(getValue)
+    this.setState({
+      text: getValue
+    })
+    list.push({// 新增加的数据加入到list数组
+      text: getValue,
+      done: false
+    });
+    localStorage.setItem('list', JSON.stringify(list));
+    // console.log(list)
+  }
+  handleChecked(sendData) {
+    setTimeout(()=> {
+      list[sendData].done = !list[sendData].done;// 根据组件传过来的索引值，修改对应数据的done值
+      localStorage.setItem('list', JSON.stringify(list));// 数据保存到localStorage
+      this.setState({
+        list: list
+      });
+    }, 200);
   }
   render() {
     return (
       <div>
-        <p>Todo列表</p>
-        <p>完成</p>
-        {this.state.list.map((data, i) => (
-          data.done
-          ?
-            <Item key={i} index={i} text={data.text} id={data.id} done={data.done} handleChecked={this.handleChecked.bind(this)}/>
-          :
-          ''
-        ))}
+        <p className="title">Todo列表</p>
+        <hr/>
+        <InpuText getInputValue={this.getInputValue.bind(this)} />
+        <p>组件获取到的数据：{this.state.text}</p>
         <p>未完成</p>
         {this.state.list.map((data, i) => (
           data.done
           ?
             ''
           :
-            <Item key={i} index={i} text={data.text} id={data.id} done={data.done} handleChecked={this.handleChecked.bind(this)}/>
-          
+            <Item key={i}
+              index={i}
+              text={data.text}
+              id={data.id}
+              done={data.done}
+              handleChecked={this.handleChecked.bind(this)}
+            />
+        ))}
+        <p>完成</p>
+        {this.state.list.map((data, i) => (
+          data.done
+          ?
+            <Item key={i}
+              index={i}
+              text={data.text}
+              id={data.id}
+              done={data.done}
+              handleChecked={this.handleChecked.bind(this)}
+            />
+          :
+          ''
         ))}
       </div>
     )
--- React/notebook/src/note/component/Item.js
+++ React/notebook/src/note/component/Item.js
@@ -1,16 +1,14 @@
 import React, { Component } from 'react';
 
 class Item extends Component {
-  // constructor(props) {
-  //   super(props)
-  // }
   handle() {
     this.props.handleChecked(this.props.index)
   }
   render() {
     let judge = this.props.done ? '完成' : '未完成'
+    let line = this.props.done ? 'done' : ''
     return (
-      <div>{this.props.text}:{this.props.id}:{judge}:
+      <div className={line}>{this.props.text}:{judge}:
         <input type="checkbox" onClick={this.handle.bind(this)} />
       </div>
     )
