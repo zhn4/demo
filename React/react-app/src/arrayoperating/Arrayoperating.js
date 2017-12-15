36b51d3eb9edda2cc2e24283c8849868857f02c9
zhou <zhouhaonan4@gmail.com>
2017-12-15 18:33:14 +0800

    fix delete functino

--- React/react-app/src/arrayoperating/Arrayoperating.js
+++ React/react-app/src/arrayoperating/Arrayoperating.js
@@ -229,6 +229,48 @@ export default class Arrayoperating extends Component {
       round: round
     }
   }
+  handleNewRound() {
+    console.log('增加round')
+    let data = this.state.array_data
+    let new_round = []
+    data.push(new_round)// 增加round
+    this.setState({
+      array_data: data
+    })
+  }
+  handleDeleteRound(i, e) {
+    console.log('删除round')
+    console.log('round index ' + i)// i是Round的索引值
+    let data = this.state.array_data
+    data.splice(i, 1)// 删除round
+    this.setState({
+      array_data: data
+    })
+  }
+  handleDeleteGroup(i, e) {
+    console.log('删除Group')
+    console.log('round index ' + i)
+    console.log('group index ' + e.target.dataset.group_index)
+    let data = this.state.array_data
+    let round = data[i]
+    round.splice(e.target.dataset.group_index, 1)// 删除group
+    this.setState({
+      array_data: data
+    })
+  }
+  handleDeleteTeam(i, e) {
+    console.log('删除Team')
+    console.log('round index ' + i)
+    console.log('group index ' + e.target.dataset.group_index)
+    console.log('team index ' + e.target.dataset.team_index)
+    let data = this.state.array_data
+    let round = data[i]
+    let group = round[e.target.dataset.group_index]
+    group.splice(e.target.dataset.team_index, 1)// 删除team
+    this.setState({
+      array_data: data
+    })
+  }
   render() {
     return (
       <div className="array">
@@ -242,11 +284,14 @@ export default class Arrayoperating extends Component {
           {/* <Team team={this.state.team}/> */}
           {/* <Group group={this.state.group}/> */}
           {/* <Round round={this.state.round}/> */}
+          {/* i是Round的索引值 */}
           {
             this.state.array_data.map((data, i) => (
-              <Round round={data}/>
+              <Round key={i} round={data} round_index={i}     handleDeleteRound={this.handleDeleteRound.bind(this, i)} handleDeleteGroup={this.handleDeleteGroup.bind(this, i)} handleDeleteTeam={this.handleDeleteTeam.bind(this, i)}
+              />
             ))
           }
+          <Btn handleFunction={this.handleNewRound.bind(this)} text="增加Round"/>
         </div>
         :
         <div>没有数据</div>
@@ -256,15 +301,28 @@ export default class Arrayoperating extends Component {
   }
 }
 
+class Btn extends Component {
+  render() {
+    return (
+      <button onClick={this.props.handleFunction}
+              data-round_index={this.props.round_index} data-group_index={this.props.group_index} data-team_index={this.props.team_index}
+      >{this.props.text}</button>
+    )
+  }
+}
+
 class Round extends Component {
   render() {
     return (
       <div className="round">
-        <div>Round</div>
+        <div>
+          Round {this.props.round_index + 1}
+          <Btn text="删除" handleFunction={this.props.handleDeleteRound} />
+        </div>
         {
           this.props.round.map((data, i) => (
             // <div key={i}>321</div>
-            <Group key={i} group={data} />
+            <Group key={i} group={data} group_index={i} handleDeleteGroup={this.props.handleDeleteGroup} handleDeleteTeam={this.props.handleDeleteTeam} />
           ))
         }
       </div>
@@ -276,11 +334,14 @@ class Group extends Component {
   render() {
     return (
       <div className="group">
-        <div>Group</div>
+        <div>
+          Group {this.props.group_index + 1}
+          <Btn text="删除" handleFunction={this.props.handleDeleteGroup} group_index={this.props.group_index} />
+        </div>
         {
           this.props.group.map((data, i) => (
             // <div key={i}>123</div>
-            <Team key={i} team={data}/>
+            <Team key={i} team={data} team_index={i} handleDeleteTeam={this.props.handleDeleteTeam} group_index={this.props.group_index}/>
           ))
         }
       </div>
@@ -292,7 +353,10 @@ class Team extends Component {
   render() {
     return (
       <div className="team">
-        <div>Team</div>
+        <div>
+          Team {this.props.team_index + 1}
+          <Btn text="删除" handleFunction={this.props.handleDeleteTeam} group_index={this.props.group_index} team_index={this.props.team_index} />
+        </div>
         {
           this.props.team.map((data, i) => (
             <Person key={i} name={data.name} score={data.score}/>
